9c0eacfd82b4dd18ad658710fb3d6e81cbbf11fb
Titoputraaa <titobamulo13@gmail.com>
2025-01-03 22:55:28 +0700

    start queue

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+// membuat struktur data Queue
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+    static int[] queue = new int[5];
+    static int front = 0;
+    static int tail = -1;
+
+
+}
