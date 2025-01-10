69d0076a0becd688592bdcd56e7c30e3fd952901
Titoputraaa <titobamulo13@gmail.com>
2025-01-10 16:37:03 +0700

    structure queue is done

--- Sircuit.java
+++ Sircuit.java
@@ -1,3 +1,5 @@
+import java.sql.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sircuit {
@@ -7,9 +9,53 @@ public class Sircuit {
 
 
     static boolean isFull() {
-        return (rear + 1) == front;
+        return (rear + 1) % queue.length == front;
     }
 
-    static boolean is
+    static boolean isEmpty() {
+        return front == -1;
     }
+
+    // menambahkan value
+    static void enQueue(int value){
+        if(isFull()) {
+            System.out.println("this queue is full");
+        } else {    
+            if (isEmpty()){
+                front = 0;
+            }
+            rear = (rear + 1) % queue.length;
+            queue[rear] = value;
+            System.out.println(Arrays.toString(queue));
+        }
+
+    }
+
+    // menghapus value 
+    static void deQueue() {
+        if (isEmpty()) {
+            System.out.println("this queue is empty");
+        } else {
+            int del = queue[front];
+            queue[front] = 0;
+            if (front == rear ) { // jika true berarti queue is empty
+                front = rear = -1;
+            } else {
+                front = (front + 1) % queue.length;
+            }
+        }
+        System.out.println(Arrays.toString(queue));
+    }
+
+    public static void main(String[] args) {
+        enQueue(1);
+        enQueue(2);
+        enQueue(3);
+        enQueue(4);
+        enQueue(5);
+        deQueue();
+        enQueue(5);
+
+    }
+}
    
