e28db2b0fb5f841396a42b3abf45e20a08943e9c
Titoputraaa <titobamulo13@gmail.com>
2025-01-10 16:10:05 +0700

    start sircuit queue

--- Sircuit.java
+++ Sircuit.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class Sircuit {
-    
-}
+    static int[] queue = new int[4];
+    static int front = -1;
+    static int rear = -1;
+
+
+    static boolean isFull() {
+        return (rear + 1) == front;
+    }
+
+    static boolean is
+    }
+   
